1bda9bc5bc96941f5d27a0ce8b3c9aced46d8bd0
devanwar <anwar@unlocklive.com>
2022-12-13 11:52:37 +0600

    added breack point in settings object to make responsive slider

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,17 @@ export default function Home() {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 5,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   return (
     <div className="fruits__container">
